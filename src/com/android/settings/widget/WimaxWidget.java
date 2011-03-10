a74ba3d59810e2fd1917dc4d8522a4d34166dc1e
andrew.boren <andrew.boren@gmail.com>
2011-03-09 18:55:03 -0700

    Start wimax standalone widget.
    Change-Id: Ia8062382d7b66d835122e4aa11760fb364bf8f32

---
+++ src/com/android/settings/widget/WimaxWidget.java
@@ -0,0 +1,17 @@
+package com.android.settings.widget;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+import com.android.settings.R;
+
+public class WimaxWidget extends Activity{
+	
+
+	protected void onCreate(Bundle savedInstanceState) {
+		
+
+	}
+
+	
+}
---
+++ src/com/android/settings/widget/WimaxWidgetProvider.java
@@ -0,0 +1,12 @@
+package com.android.settings.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+public class WimaxWidgetProvider extends AppWidgetProvider {
+
+	
+
+
+}
