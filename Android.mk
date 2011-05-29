4f6685fb40bd1b1d503a823fc5f456ce0c532f51
andrew.boren <andrew.boren@gmail.com>
2011-05-28 22:25:10 -0700

    Hacky workaround for for device-specific widgets.
    Change-Id: Id8e89814ac85516f96d719afbac0a028da0e21bf

--- Android.mk
+++ Android.mk
@@ -1,6 +1,14 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
+ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),harmony)
+LOCAL_MANIFEST_FILE := /no4g_nogps/AndroidManifest.xml
+else ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),speedy)
+LOCAL_MANIFEST_FILE := /4g/AndroidManifest.xml
+else ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),supersonic)
+LOCAL_MANIFEST_FILE := /4g/AndroidManifest.xml
+endif
+
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
@@ -12,3 +20,5 @@ include $(BUILD_PACKAGE)
 
 # Use the folloing include to make our test apk.
 include $(call all-makefiles-under,$(LOCAL_PATH))
+
+
