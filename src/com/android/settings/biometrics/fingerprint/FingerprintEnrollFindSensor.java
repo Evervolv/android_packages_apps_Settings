506d3ad13bf720bcbcdf4c4f73e12adec0138204
d34d <clark@cyngn.com>
2016-06-23 11:21:45 -0700

    fingerprint: Allow devices to configure sensor location
    This is a squash of the following changes:
    
    Author: d34d <clark@cyngn.com>
    Date:   2016-06-23 11:21:45 -0700
    
        fingerprint: Allow devices to configure sensor location
    
        The text for enrolling a fingerprint always assumes the sensor is
        located on the back.  This patch adds a config integer that defines
        where the sensor is located on the device.
          0 = back
          1 = front
          2 = left side
          3 = right side
    
        This patch also adds a new drawable that shows the front of the device
        when applicable.
    
        Change-Id: Ia8f5f8ff4ba4d13d35aed3052f60ff665dd4f294
        TICKET: CYNGNOS-3089
    
    Author: Paul Keith <javelinanddart@gmail.com>
    Date:   2018-01-26 22:23:03 +0100
    
        FingerprintEnrollFindSensor: Don't overlay front for side sensors
    
        * Both the base image and the base image + overlay are equally nonsense
          for devices with a side fingerprint scanner, but for those that overlay
          a custom fingerprint enrollment graphic, this image overlay wreaks havoc
        * Only set this overlay to be visible if the sensor is on the front, to
          allow devices with side sensors to only overlay the base image
    
        Change-Id: I7cbcea4830d4526cf9f7d139e7f84f21117fa9c4
    
    Author: Michael W <baddaemon87@gmail.com>
    Date:   Thu Jan 17 21:44:28 2019 +0100
    
        Settings: Make find fingerprint look great again
    
        * The current logic places the animation based on the size of
          the whole overlay instead of the image, which is a fixed-size png
        * Get the size of the image instead and place the dot relative to it
        * Also use "wrap_content" and add a padding to the top so the
          distance to the text is a fixed thing, not depending on how
          much space relative to the wanted space it actually takes
    
        * Preview: https://imgur.com/a/XanJ2aP
    
        Change-Id: I2bd08cee1abd1c6bad78ca1efc2189e573ded3cc
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Sat Dec 15 17:11:41 2018 +0100
    
        fingerprint: Remove unnecessary spacing in enroll layout
    
        * This makes it look ugly on 480 dpi devices.
    
        Change-Id: Ia1bbd443063a125878dddd12e5c6a816d289fb82
    
    Author: Bruno Martins <bgcngm@gmail.com>
    Date:   Sat Sep 21 12:54:33 2019 +0100
    Edit: Adapted to Q
    
        Settings: add fod option in FP location overlay
    
        we dont want any drawable if the position is FOD
    
        Change-Id: Id953436658b241dfa703a95247beebbf036e2558
    
    From: Marko Man <darkobas@gmail.com>
    Date: Wed, 6 Nov 2019 09:09:14 +0100
    
    Change-Id: I203620fd34220da061e8c1e5817dd32cdfa6783f

--- src/com/android/settings/biometrics/fingerprint/FingerprintEnrollFindSensor.java
+++ src/com/android/settings/biometrics/fingerprint/FingerprintEnrollFindSensor.java
@@ -21,6 +21,8 @@ import android.content.Intent;
 import android.hardware.fingerprint.FingerprintManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 
@@ -37,6 +39,12 @@ import com.google.android.setupcompat.template.FooterButton;
  * Activity explaining the fingerprint sensor location for fingerprint enrollment.
  */
 public class FingerprintEnrollFindSensor extends BiometricEnrollBase {
+
+    private static final int SENSOR_LOCATION_BACK = 0;
+    private static final int SENSOR_LOCATION_FRONT = 1;
+    private static final int SENSOR_LOCATION_LEFT = 2;
+    private static final int SENSOR_LOCATION_RIGHT = 3;
+    private static final int SENSOR_LOCATION_SCREEN = 4;
 
     @Nullable
     private FingerprintFindSensorAnimation mAnimation;
@@ -68,6 +76,25 @@ public class FingerprintEnrollFindSensor extends BiometricEnrollBase {
             mAnimation = (FingerprintFindSensorAnimation) animationView;
         } else {
             mAnimation = null;
+        }
+
+        int sensorLocation = getResources().getInteger(R.integer.config_fingerprintSensorLocation);
+        if (sensorLocation < SENSOR_LOCATION_BACK || sensorLocation > SENSOR_LOCATION_SCREEN) {
+            sensorLocation = SENSOR_LOCATION_BACK;
+        }
+        final String location = getResources().getStringArray(
+                R.array.security_settings_fingerprint_sensor_locations)[sensorLocation];
+        TextView message = (TextView) findViewById(R.id.sud_layout_description);
+        message.setText(getString(
+                R.string.security_settings_fingerprint_enroll_find_sensor_message_cm, location));
+        if (sensorLocation == SENSOR_LOCATION_FRONT) {
+            findViewById(R.id.fingerprint_sensor_location_front_overlay)
+                    .setVisibility(View.VISIBLE);
+        }
+        if (sensorLocation == SENSOR_LOCATION_SCREEN) {
+            findViewById(R.id.fingerprint_sensor_location_front_overlay)
+                    .setVisibility(View.VISIBLE);
+            animationView.setVisibility(View.GONE);
         }
     }
 
