971696f0228ba6633b52700b27684b8f1d889f8a
Bruno Martins <bgcngm@gmail.com>
2017-12-02 12:52:18 +0000

    Settings: Add proximity check on wake preference
     * Splitted from commit 5aa2f34b1c78c5b41f25b820c1f29b29c58e0cae
       and adapted to the Lineage SDK.
    
    Change-Id: I7516c833ff39de6bdb36c815a5292606cbaa0533

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -44,6 +44,7 @@ import java.util.List;
 public class DisplaySettings extends DashboardFragment {
     private static final String TAG = "DisplaySettings";
 
+    private static final String KEY_PROXIMITY_ON_WAKE = "proximity_on_wake";
     private static final String KEY_SCREEN_TIMEOUT = "screen_timeout";
 
     @Override
@@ -95,6 +96,16 @@ public class DisplaySettings extends DashboardFragment {
 
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
             new BaseSearchIndexProvider(R.xml.display_settings) {
+
+                @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    List<String> keys = super.getNonIndexableKeys(context);
+                    if (!context.getResources().getBoolean(
+                            com.evervolv.platform.internal.R.bool.config_proximityCheckOnWake)) {
+                        keys.add(KEY_PROXIMITY_ON_WAKE);
+                    }
+                    return keys;
+                }
 
                 @Override
                 public List<AbstractPreferenceController> createPreferenceControllers(
