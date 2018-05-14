7d81efee5efe5ae1afb8662fa9f9f0a2257355e9
Rashed Abdel-Tawab <rashed@linux.com>
2018-05-14 14:35:32 -0700

    Settings: add Reading Mode support
    Change-Id: I36e473ccd30be6799a1cff797d6b7dd63fbb2db5

---
+++ src/com/android/settings/display/ReadingModePreferenceController.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.android.settings.display;
+
+import android.content.Context;
+
+import androidx.preference.Preference;
+
+import com.android.settings.R;
+import com.android.settings.core.TogglePreferenceController;
+
+import evervolv.hardware.HardwareManager;
+
+public class ReadingModePreferenceController extends TogglePreferenceController implements
+        Preference.OnPreferenceChangeListener {
+
+    private final HardwareManager mHardware;
+
+    public ReadingModePreferenceController(Context context, String key) {
+        super(context, key);
+        mHardware = HardwareManager.getInstance(context);
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        super.updateState(preference);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return mHardware.isSupported(HardwareManager.FEATURE_READING_ENHANCEMENT)
+                ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
+    }
+
+    @Override
+    public int getSliceHighlightMenuRes() {
+        return R.string.menu_key_display;
+    }
+
+    @Override
+    public boolean isChecked() {
+        return mHardware.get(HardwareManager.FEATURE_READING_ENHANCEMENT);
+    }
+
+    @Override
+    public boolean setChecked(boolean isChecked) {
+        return mHardware.set(HardwareManager.FEATURE_READING_ENHANCEMENT, isChecked);
+    }
+}
