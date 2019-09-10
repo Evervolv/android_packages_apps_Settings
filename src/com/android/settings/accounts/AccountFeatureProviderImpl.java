7f01fb8d97bd3777916579a2e8c7f2a90a034b51
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-10 12:20:19 -0300

    Settings: Set the default account provider to Google
    Change-Id: I4cedd3ea99be911e874270bee86f9c5447cc4268
    Signed-off-by: Adithya <gh0strider.2k18.reborn@gmail.com>
    
    Settings: Show user's Google avatar on the homepage
    
    Change-Id: I16f3e241d2d2a4a25977fe7428687b88c8fde0c6
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>

--- src/com/android/settings/accounts/AccountFeatureProviderImpl.java
+++ src/com/android/settings/accounts/AccountFeatureProviderImpl.java
@@ -1,16 +1,28 @@
 package com.android.settings.accounts;
 
 import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.content.Context;
+
+import com.android.settings.R;
+import com.android.settings.overlay.FeatureFactory;
 
 public class AccountFeatureProviderImpl implements AccountFeatureProvider {
     @Override
     public String getAccountType() {
+        final String accountType = FeatureFactory.getAppContext().getString(R.string.account_type);
+        if (!accountType.isEmpty()) {
+            return accountType;
+        }
         return null;
     }
 
     @Override
     public Account[] getAccounts(Context context) {
+        final String type = getAccountType();
+        if (type != null) {
+            return AccountManager.get(context).getAccountsByType(getAccountType());
+        }
         return new Account[0];
     }
 }
