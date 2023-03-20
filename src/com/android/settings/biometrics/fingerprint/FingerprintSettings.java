30e565285392f9bd8ea809e744c22e9960fbe8b8
Michael Bestas <mkbestas@lineageos.org>
2023-03-20 02:40:07 +0200

    Hide SFPS wakeup setting when it's not supported
    Change-Id: I22d4eb745379a6ca56b8fe23e1584131c4e945a6

--- src/com/android/settings/biometrics/fingerprint/FingerprintSettings.java
+++ src/com/android/settings/biometrics/fingerprint/FingerprintSettings.java
@@ -196,7 +196,8 @@ public class FingerprintSettings extends SubSettings {
             if (manager == null || !manager.isHardwareDetected()) {
                 return null;
             }
-            if (manager.isPowerbuttonFps()) {
+            if (manager.isPowerbuttonFps() && context.getResources().getBoolean(
+                    com.evervolv.platform.internal.R.bool.config_fingerprintWakeAndUnlock)) {
                 controllers.add(
                         new FingerprintUnlockCategoryController(
                                 context,
@@ -268,6 +269,7 @@ public class FingerprintSettings extends SubSettings {
         private PreferenceCategory mFingerprintsEnrolledCategory;
         private PreferenceCategory mFingerprintUnlockCategory;
         private PreferenceCategory mFingerprintUnlockFooter;
+        private boolean mFingerprintWakeAndUnlock;
 
         private FingerprintManager mFingerprintManager;
         private FingerprintUpdater mFingerprintUpdater;
@@ -445,6 +447,8 @@ public class FingerprintSettings extends SubSettings {
             mFingerprintManager = Utils.getFingerprintManagerOrNull(activity);
             mFingerprintUpdater = new FingerprintUpdater(activity, mFingerprintManager);
             mSensorProperties = mFingerprintManager.getSensorPropertiesInternal();
+            mFingerprintWakeAndUnlock = getContext().getResources().getBoolean(
+                    com.evervolv.platform.internal.R.bool.config_fingerprintWakeAndUnlock);
 
             mToken = getIntent().getByteArrayExtra(
                     ChooseLockSettingsHelper.EXTRA_KEY_CHALLENGE_TOKEN);
@@ -549,7 +553,7 @@ public class FingerprintSettings extends SubSettings {
                         R.string.security_fingerprint_disclaimer_lockscreen_disabled_2
                 );
                 if (helpIntent != null) {
-                    if (isSfps()) {
+                    if (isSfps() && mFingerprintWakeAndUnlock) {
                         column2.mLearnMoreOverrideText = getText(
                                 R.string.security_settings_fingerprint_settings_footer_learn_more);
                     }
@@ -632,7 +636,7 @@ public class FingerprintSettings extends SubSettings {
             // This needs to be after setting ids, otherwise
             // |mRequireScreenOnToAuthPreferenceController.isChecked| is always checking the primary
             // user instead of the user with |mUserId|.
-            if (isSfps()) {
+            if (isSfps() && mFingerprintWakeAndUnlock) {
                 scrollToPreference(fpPrefKey);
                 addFingerprintUnlockCategory();
             }
@@ -682,7 +686,9 @@ public class FingerprintSettings extends SubSettings {
 
         private void addFingerprintUnlockCategory() {
             mFingerprintUnlockCategory = findPreference(KEY_FINGERPRINT_UNLOCK_CATEGORY);
-            setupFingerprintUnlockCategoryPreferences();
+            if (mRequireScreenOnToAuthPreferenceController != null) {
+                setupFingerprintUnlockCategoryPreferences();
+            }
             final Preference restToUnlockPreference = FeatureFactory.getFeatureFactory()
                     .getFingerprintFeatureProvider()
                     .getSfpsRestToUnlockFeature(getContext())
@@ -696,7 +702,9 @@ public class FingerprintSettings extends SubSettings {
                 mRequireScreenOnToAuthPreference.setOnPreferenceChangeListener(
                         restToUnlockPreference.getOnPreferenceChangeListener());
             }
-            updateFingerprintUnlockCategoryVisibility();
+            if (mFingerprintUnlockCategoryPreferenceController != null) {
+                updateFingerprintUnlockCategoryVisibility();
+            }
         }
 
         private void updateFingerprintUnlockCategoryVisibility() {
@@ -722,7 +730,7 @@ public class FingerprintSettings extends SubSettings {
 
         private void updatePreferencesAfterFingerprintRemoved() {
             updateAddPreference();
-            if (isSfps()) {
+            if (isSfps() && mFingerprintWakeAndUnlock) {
                 updateFingerprintUnlockCategoryVisibility();
             }
             updatePreferences();
@@ -954,7 +962,8 @@ public class FingerprintSettings extends SubSettings {
         private List<AbstractPreferenceController> buildPreferenceControllers(Context context) {
             final List<AbstractPreferenceController> controllers =
                     createThePreferenceControllers(context);
-            if (isSfps()) {
+            if (isSfps() && context.getResources().getBoolean(
+                    com.evervolv.platform.internal.R.bool.config_fingerprintWakeAndUnlock)) {
                 for (AbstractPreferenceController controller : controllers) {
                     if (controller.getPreferenceKey() == KEY_FINGERPRINT_UNLOCK_CATEGORY) {
                         mFingerprintUnlockCategoryPreferenceController =
